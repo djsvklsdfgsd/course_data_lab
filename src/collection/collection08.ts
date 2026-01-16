6209d9180c70a454763fef7b6900416f6fb4c42e
klfkglf <freedom_bob@mail.ru>
2026-01-17 01:06:28 +0600

    c8

--- src/collection/collection08.ts
+++ src/collection/collection08.ts
@@ -3,7 +3,18 @@
 */
 
 export function unionSets<T>(set1: Set<T>, set2: Set<T>): Set<T> {
+	const res = new Set<T>();
+	let i;
 
+	for (i of set1) {
+		res.add(i);
+	}
+
+	for (i of set2) {
+		res.add(i);
+	}
+	
+	return res;
 }
 
 
