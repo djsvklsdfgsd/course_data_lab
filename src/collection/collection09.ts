23fa5cf8f2c17cf0f1b7d88b4d7f549479db2a9a
klfkglf <freedom_bob@mail.ru>
2026-01-17 01:36:11 +0600

    c9

--- src/collection/collection09.ts
+++ src/collection/collection09.ts
@@ -4,6 +4,15 @@
 
 export function findIntersection(arr1: number[], arr2: number[]): number[] {
   const intersection = new Set<number>();
+  let i;
+  let j;
 
+  for (i of arr1) {
+    for (j of arr2) {
+      if (i===j){
+        intersection.add(i);
+      }
+    }
+  }
   return [...intersection];
 }
