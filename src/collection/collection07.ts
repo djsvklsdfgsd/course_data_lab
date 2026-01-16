3b6c5030219db2e5fe497b20e8dcf3dc6f92f3bd
klfkglf <freedom_bob@mail.ru>
2026-01-17 00:54:46 +0600

    c7

--- src/collection/collection07.ts
+++ src/collection/collection07.ts
@@ -4,6 +4,18 @@
 
 export function countFrequency(arr: string[]): Map<string, number> {
   const map = new Map();
+  let word;
+  let count;
+  
+  for (word of arr) {
+    if (map.has(word)) {
+      count = map.get(word);
+      map.set(word, count + 1);
+    }
+    else {
+      map.set(word, 1);
+    }
+  }
 
   return map;
 }
