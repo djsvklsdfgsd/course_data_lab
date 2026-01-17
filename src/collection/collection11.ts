5202db0e1e39985774e0e8ce7815049ccc5171e4
klfkglf <freedom_bob@mail.ru>
2026-01-18 01:04:41 +0600

    c11

--- src/collection/collection11.ts
+++ src/collection/collection11.ts
@@ -3,5 +3,18 @@
 */
 
 export function groupBy<T extends Record<string, any>>(arr: T[], key: keyof T): Map<T[keyof T], T[]> {
+	const map = new Map<T[keyof T], T[]>();
 
+	for (let i = 0; i < arr.length; i++) {
+		const obj = arr[i];
+		const value = obj[key];
+
+		if (map.has(value)) {
+			map.get(value)!.push(obj);
+		}
+		else {
+			map.set(value, [obj]);
+		}
+	}
+	return map;
 }
