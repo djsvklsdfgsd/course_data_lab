52a59b2e51e04495533ff8ffae1d430acc7836cd
klfkglf <freedom_bob@mail.ru>
2026-01-19 00:41:31 +0600

    c13

--- src/collection/collection13.ts
+++ src/collection/collection13.ts
@@ -4,6 +4,20 @@
 
 export function countByRanges(numbers: number[], ranges: [number, number][]): Map<string, number> {
 	const result = new Map<string, number>();
+	
+	for (const [start, end] of ranges) {
+		const key = `${start}-${end}`;
+		result.set(key, 0);
+	}
 
+	for (const num of numbers) {
+		for (const [start, end] of ranges) {
+			if (num >= start && num <= end) {
+				const key = `${start}-${end}`;
+				const value = result.get(key)!;
+				result.set(key, value + 1);
+			}
+		}
+	}
 	return result;
 }
