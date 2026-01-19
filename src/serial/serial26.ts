8ebe3d9bda965d79482317402f4a3bfa6cbe3f6c
klfkglf <freedom_bob@mail.ru>
2026-01-20 04:04:13 +0600

    s26

--- src/serial/serial26.ts
+++ src/serial/serial26.ts
@@ -11,7 +11,12 @@ export function collectBookIds(xml: string): string[] {
   const ids: string[] = [];
   
   // TODO: При открытии тега 'book' добавляйте значение атрибута 'id' в массив ids
-
+  
+  parser.onopentag = (node) => {
+    if (node.name === 'book' && node.attributes.id) {
+      ids.push(node.attributes.id.toString());
+    }
+  };
   
   parser.write(xml).close();
   return ids;
