e956422609fee04b3c1e6e003b9d6c2ae6591a30
klfkglf <freedom_bob@mail.ru>
2026-01-19 22:41:32 +0600

    c28

--- src/collection/collection28.ts
+++ src/collection/collection28.ts
@@ -5,5 +5,8 @@
 type Product = {category: string, rating: number, name: string}
 
 export function getTopRatedInCategory(products: Product[], category: string): Product[] {
-
+	const categoryProducts = products.filter(p => p.category === category);
+	const sorted = categoryProducts.sort((a, b) => b.rating - a.rating);
+	
+	return sorted.slice(0, 3);
 }
