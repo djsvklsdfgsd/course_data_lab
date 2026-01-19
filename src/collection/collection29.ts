19353d55b97c9be80c9b632ce56f337f296d555b
klfkglf <freedom_bob@mail.ru>
2026-01-19 23:00:02 +0600

    c29

--- src/collection/collection29.ts
+++ src/collection/collection29.ts
@@ -5,5 +5,8 @@
 type User = {active: boolean, progress: number}
 
 export function calculateTotalProgress(users: User[]): number {
+	const filtration = users.filter(user => user.active && user.progress > 50);
+	const total = filtration.reduce((sum, user) => sum + user.progress, 0);
 
+	return total / filtration.length;
 }
