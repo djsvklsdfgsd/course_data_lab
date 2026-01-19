9f1cc1f768d9eb9ccc32a384f4e8c325563e1a8b
klfkglf <freedom_bob@mail.ru>
2026-01-19 19:37:26 +0600

    c25

--- src/collection/collection25.ts
+++ src/collection/collection25.ts
@@ -6,5 +6,22 @@ type Order = {status: string, amount: number}
 type Stats = {total: number, count: number, average: number}
 
 export function getOrderStats(orders: Order[]): Stats{
+	let total = 0;
+	let count = 0;
 
+	for (let i = 0; i <orders.length; i++) {
+		const order = orders[i]
+
+		if (order.status === 'completed') {
+			total += order.amount;
+			count++;
+		}
+	}
+	
+	let average = 0;
+    if (count > 0) {
+        average = total / count;
+    }
+
+	return { total, count, average };
 }
