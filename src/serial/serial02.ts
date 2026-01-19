84cfca7211b5271ba460b5c7b18d4d1bab795266
klfkglf <freedom_bob@mail.ru>
2026-01-20 00:26:02 +0600

    s2

--- src/serial/serial02.ts
+++ src/serial/serial02.ts
@@ -2,10 +2,14 @@
 	Модифицируйте класс Team, чтобы массив members корректно восстанавливался как массив объектов User.
 */
 
+import "reflect-metadata";
 import { User } from "./user";
+import { Type } from "class-transformer";
 
 export class Team {
   name: string;
+    
+  @Type(() => User)
   members: User[]; // Требует декоратора
   
   constructor(name: string, members: User[]) {
