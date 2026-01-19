2fa9c4d708032d9690acd504f5d00ee12d6e4e45
klfkglf <freedom_bob@mail.ru>
2026-01-20 01:07:39 +0600

    s9

--- src/serial/serial09.ts
+++ src/serial/serial09.ts
@@ -2,10 +2,13 @@
 	Исправьте структуру "дерево" с рекурсивными ссылками для корректной десериализации
 */
 
+import "reflect-metadata";
+import { Type } from "class-transformer";
 
 export class TreeNode {
   value: string;
   
+  @Type(() => TreeNode)
   children: TreeNode[];
 
   constructor(value: string) {
