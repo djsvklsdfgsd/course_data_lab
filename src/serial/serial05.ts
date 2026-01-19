5bf7a412932eadc3cddcea8d0650430b8b8dfabc
klfkglf <freedom_bob@mail.ru>
2026-01-20 00:30:56 +0600

    s5

--- src/serial/serial05.ts
+++ src/serial/serial05.ts
@@ -2,10 +2,14 @@
 	Добавьте декоратор для преобразования строки в объект Date.
 */
 
+import "reflect-metadata";
 import { User } from "./user";
+import { Type } from "class-transformer";
 
 export class Session {
+  @Type (() => User)
   user: User; // Требует декоратора
+  @Type (() => Date)
   loginAt: Date; // Требует декоратора Date
 
   constructor(user: User, loginAt: Date) {
