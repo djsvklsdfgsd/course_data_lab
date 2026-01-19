7f3412bbb41af9f94ab7d61d0b79fc42d3709137
klfkglf <freedom_bob@mail.ru>
2026-01-20 01:00:29 +0600

    s8

--- src/serial/serial08.ts
+++ src/serial/serial08.ts
@@ -2,6 +2,9 @@
 	Обеспечьте сериализацию и десериализацию объектов классов Circle, Rectangle и Drawing
 */
 
+import "reflect-metadata";
+import { Type, Transform } from "class-transformer";
+
 export abstract class Shape {
   abstract area(): number;
 }
@@ -33,7 +36,21 @@ export class Rectangle extends Shape {
 export class Drawing {
   name: string;
 
+  @Transform(({ value }) => {
+    if (!value || !Array.isArray(value)) return [];
+    
+    return value.map((item: any) => {
+      if (item.radius !== undefined) {
+        return Object.assign(new Circle(0), item);
+      } else if (item.width !== undefined && item.height !== undefined) {
+        return Object.assign(new Rectangle(0, 0), item);
+      }
+      return item;
+    });
+  }, { toClassOnly: true })
+  @Type(() => Shape)
   shapes: Shape[];
+  
   constructor(name: string, shapes: Shape[]) {
     this.name = name;
     this.shapes = shapes;
