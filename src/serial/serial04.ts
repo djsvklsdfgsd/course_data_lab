7c473a9f097da292d2d3713268dd0018eeb1c721
klfkglf <freedom_bob@mail.ru>
2026-01-20 00:29:53 +0600

    s4

--- src/serial/serial04.ts
+++ src/serial/serial04.ts
@@ -2,11 +2,15 @@
 	Настройте класс Project с помощью декораторов для работы с разными типами вложенных данных.
 */
 
-import  { User } from "./user";
+import "reflect-metadata";
+import { User } from "./user";
+import { Type } from "class-transformer";
 
 export class Project {
   name: string;
+  @Type(() => User)
   lead: User; // Требует декоратора
+  @Type(() => User)
   developers: User[]; // Требует декоратора
   constructor(name: string, lead: User, developers: User[]) {
 	this.name = name;
