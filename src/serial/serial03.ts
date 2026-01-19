77329cc28bd1b03fab87ca33dd6d408071509e36
klfkglf <freedom_bob@mail.ru>
2026-01-20 00:28:39 +0600

    s3

--- src/serial/serial03.ts
+++ src/serial/serial03.ts
@@ -2,10 +2,13 @@
 	Добавьте необходимые декораторы в класс Department, чтобы вложенные сотрудники восстанавливались правильно.
 */
 
+import "reflect-metadata";
 import { User } from "./user";
+import { Type } from "class-transformer";
 
 export class Department {
   name: string;
+  @Type(() => User)
   employees: User[]; // Требует декоратора
   constructor(name: string, employees: User[]) {
     this.name = name;
@@ -15,6 +18,7 @@ export class Department {
 
 export class Company {
   name: string;
+  @Type(() => Department)
   departments: Department[]; // Требует декоратора
   constructor(name: string, departments: Department[]) {
     this.name = name;
