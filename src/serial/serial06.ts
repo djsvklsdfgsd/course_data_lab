c411cd97aee10f1c2f008edc1a3cf3bdd32d3dc3
klfkglf <freedom_bob@mail.ru>
2026-01-20 00:32:46 +0600

    s6

--- src/serial/serial06.ts
+++ src/serial/serial06.ts
@@ -2,10 +2,13 @@
 	Настройте класс так, чтобы опциональное поле manager восстанавливалось как User или оставалось null.
 */
 
+import "reflect-metadata";
 import { User } from "./user";
+import { Type } from "class-transformer";
 
 export class Employee {
   name: string;
+  @Type (() => User)
   manager: User | null; // Требует декоратора для типа
 
   constructor(name: string, manager: User | null) {
