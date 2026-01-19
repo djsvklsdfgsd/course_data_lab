f3ebfb15366ccfe0bf0df9978cfea846618d288d
klfkglf <freedom_bob@mail.ru>
2026-01-19 20:30:45 +0600

    c27

--- src/collection/collection27.ts
+++ src/collection/collection27.ts
@@ -4,5 +4,27 @@
 type Post = { tags: string[] };
 
 export function findMostPopularTag(posts: Post[]): Record<string, number> {
+    const allTags = posts.flatMap(post => post.tags);
+    const counts: Record<string, number> = {};
+    
+    allTags.forEach(tag => {
+        if (!counts.hasOwnProperty(tag)) {
+            counts[tag] = 1;
+        } else {
+            counts[tag] = counts[tag] + 1;
+        }
+    });
 
+    const tags = Object.keys(counts);
+    let bestTag = tags[0] || "";
+    let bestCount = counts[bestTag] || 0;
+    
+    tags.forEach(tag => {
+        if (counts[tag] > bestCount) {
+            bestTag = tag;
+            bestCount = counts[tag];
+        }
+    });
+    
+    return counts;
 }
