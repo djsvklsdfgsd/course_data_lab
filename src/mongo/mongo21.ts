20ecabb1ec3d91693bd6caca42d0d0b72dec1fb9
klfkglf <freedom_bob@mail.ru>
2026-01-21 00:43:57 +0600

    m21

--- src/mongo/mongo21.ts
+++ src/mongo/mongo21.ts
@@ -23,7 +23,43 @@ export async function get_customer_categories(db: Db): Promise<CustomerCategory[
     // "VIP" если totalPurchases > 1000 ИЛИ membershipLevel = "premium"
     // "Regular" если totalPurchases между 100 и 1000
     // "New" в остальных случаях
-	return await db.collection("customers").aggregate([
-
+    return await db.collection("customers").aggregate([
+        {
+            $addFields: {
+                category: {
+                    $switch: {
+                        branches: [
+                            {
+                                case: {
+                                    $or: [
+                                        { $gt: ["$totalPurchases", 1000] },
+                                        { $eq: ["$membershipLevel", "premium"] }
+                                    ]
+                                },
+                                then: "VIP"
+                            },
+                            {
+                                case: {
+                                    $and: [
+                                        { $gte: ["$totalPurchases", 100] },
+                                        { $lte: ["$totalPurchases", 1000] }
+                                    ]
+                                },
+                                then: "Regular"
+                            }
+                        ],
+                        default: "New"
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                name: 1,
+                totalPurchases: 1,
+                membershipLevel: 1,
+                category: 1
+            }
+        }
     ]).toArray() as CustomerCategory[]
 }
