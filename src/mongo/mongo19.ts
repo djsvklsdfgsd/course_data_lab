1d73437cd2be40e4f54d3715f8d625c13e470e84
klfkglf <freedom_bob@mail.ru>
2026-01-20 23:58:27 +0600

    m19

--- src/mongo/mongo19.ts
+++ src/mongo/mongo19.ts
@@ -25,7 +25,27 @@ export interface OrderTotal {
 export async function get_order_totals(db: Db): Promise<OrderTotal[]> {
 	// TODO: Рассчитать итоговую сумму для каждого заказа с учетом скидки
 	// Поля: product, total (quantity * unitPrice), finalTotal (total - discount)
-	return await db.collection("orders").aggregate([
-
-	]).toArray() as OrderTotal[]
+    return await db.collection("orders").aggregate([
+        {
+            $addFields: {
+                total: { $multiply: ["$quantity", "$unitPrice"] },
+                finalTotal: { 
+                    $subtract: [
+                        { $multiply: ["$quantity", "$unitPrice"] },
+                        "$discount"
+                    ]
+                }
+            }
+        },
+        {
+            $project: {
+                product: 1,
+                quantity: 1,
+                unitPrice: 1,
+                discount: 1,
+                total: 1,
+                finalTotal: 1
+            }
+        }
+    ]).toArray() as OrderTotal[]
 }
