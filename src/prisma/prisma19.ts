8283b0cc2e70d9ae9ebdb0ea4ba293ab03caf4c5
klfkglf <freedom_bob@mail.ru>
2026-01-21 04:57:25 +0600

    p19

--- src/prisma/prisma19.ts
+++ src/prisma/prisma19.ts
@@ -4,4 +4,32 @@ export async function find_most_popular_course() {
     // TODO: Найти курс с максимальным количеством уникальных студентов
     // Использовать агрегацию средствами Prisma (groupBy)
     // Вернуть объект курса с дополнительным полем studentCount
+    const grouped = await prisma.grade.groupBy({
+        by: ['courseId'],
+        _count: {
+            studentId: true
+        }
+    });
+    
+    if (grouped.length === 0) return null;
+
+    let maxCourse = grouped[0];
+    for (const course of grouped) {
+        if (course._count.studentId > maxCourse._count.studentId) {
+            maxCourse = course;
+        }
+    }
+
+    const course = await prisma.course.findUnique({
+        where: { 
+            id: maxCourse.courseId 
+        }
+    });
+    
+    if (!course) return null;
+    
+    return {
+        ...course,
+        studentCount: maxCourse._count.studentId
+    };
 }
