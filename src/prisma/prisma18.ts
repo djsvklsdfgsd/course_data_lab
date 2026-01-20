e984f44e51f153423f90deb8da26112ab2c18317
klfkglf <freedom_bob@mail.ru>
2026-01-21 04:45:44 +0600

    p18

--- src/prisma/prisma18.ts
+++ src/prisma/prisma18.ts
@@ -4,4 +4,13 @@ export async function find_students_with_excellent_grades() {
     // TODO: Найти всех студентов, у которых есть хотя бы одна оценка 5
     // Использовать обработку данных в TypeScript (не фильтрацию Prisma)
     // Вернуть массив студентов с информацией о person
+    const allStudents = await prisma.student.findMany({
+        include: {
+            person: true,
+            grades: true
+        }
+    });
+    
+    return allStudents.filter(student => student.grades.some(grade => grade.grade === 5)
+    );
 }
