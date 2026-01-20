57f30e064de3b00b36b5f7258c3a914babf7ea88
klfkglf <freedom_bob@mail.ru>
2026-01-21 05:36:34 +0600

    p23

--- src/prisma/prisma23.ts
+++ src/prisma/prisma23.ts
@@ -4,4 +4,20 @@ export async function delete_students_without_person() {
     // TODO: Удалить студентов, у которых нет связанной записи в person
     // Использовать фильтрацию средствами Prisma
     // Вернуть количество удаленных записей
+    const allPersons = await prisma.person.findMany({
+        select: { 
+            id: true 
+        }
+    });
+    const personIds = allPersons.map(p => p.id);
+
+    const result = await prisma.student.deleteMany({
+        where: {
+            personId: {
+                notIn: personIds 
+            }
+        }
+    });
+    
+    return result.count;
 }
