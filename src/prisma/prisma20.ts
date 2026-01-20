653834eaf07156a71ff46801ae3afa6b0fb91888
klfkglf <freedom_bob@mail.ru>
2026-01-21 05:10:02 +0600

    p20

--- src/prisma/prisma20.ts
+++ src/prisma/prisma20.ts
@@ -4,4 +4,30 @@ export async function update_student_names_pattern(oldPattern: string, newPatter
     // TODO: Обновить имена студентов, заменяя oldPattern на newPattern
     // Использовать обработку в TypeScript с отдельными запросами update
     // Вернуть количество обновленных студентов
+    const students = await prisma.student.findMany({
+        include: { 
+            person: true 
+        }
+    });
+    
+    let count = 0;
+    
+    for (const student of students) {
+        if (student.person.name.includes(oldPattern)) {
+            const newName = student.person.name.replace(oldPattern, newPattern);
+            
+            await prisma.person.update({
+                where: { 
+                    id: student.person.id 
+                },
+                data: { 
+                    name: newName 
+                }
+            });
+            
+            count++;
+        }
+    }
+    
+    return count;
 }
