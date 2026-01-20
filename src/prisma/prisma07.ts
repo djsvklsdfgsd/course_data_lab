acc9d58de91235cab146d42c2594a36d29e7ccf3
klfkglf <freedom_bob@mail.ru>
2026-01-21 03:29:52 +0600

    p7

--- src/prisma/prisma07.ts
+++ src/prisma/prisma07.ts
@@ -6,4 +6,19 @@ export async function update_student_email(
 ) {
   // TODO: Обновить email студента по его ID
   // Вернуть обновленного студента с информацией о person
+  return await prisma.student.update({
+    where: {
+      id: studentId
+    },
+    data: {
+      person: {
+        update: {
+          email: newEmail
+        }
+      }
+    },
+    include: {
+      person: true
+    }
+  });
 }
