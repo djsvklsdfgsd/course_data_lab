7b170418917b72127fc763918b090922633ddace
klfkglf <freedom_bob@mail.ru>
2026-01-21 03:33:06 +0600

    p8

--- src/prisma/prisma08.ts
+++ src/prisma/prisma08.ts
@@ -10,4 +10,21 @@ export interface CourseWithStudentCount {
 export async function find_courses_with_student_count(): Promise<CourseWithStudentCount[]> {
     // TODO: Найти все курсы с количеством уникальных студентов, имеющих оценки по этому курсу
     // Вернуть массив курсов с дополнительным полем studentCount
+        const courses = await prisma.course.findMany({
+        include: {
+            grades: {
+                select: {
+                    studentId: true
+                },
+                distinct: ['studentId'] 
+            }
+        }
+    });
+    
+    return courses.map(course => ({
+        id: course.id,
+        title: course.title,
+        description: course.description,
+        studentCount: course.grades.length 
+    }));
 }
