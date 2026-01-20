bb9724baeac2e478b13a384c104358deecce8b2e
klfkglf <freedom_bob@mail.ru>
2026-01-21 04:41:44 +0600

    p16

--- src/prisma/prisma16.ts
+++ src/prisma/prisma16.ts
@@ -4,4 +4,26 @@ export async function find_students_with_most_courses() {
     // TODO: Найти студентов с максимальным количеством уникальных курсов
     // Использовать обработку данных в TypeScript (не агрегацию Prisma)
     // Вернуть массив студентов с информацией о person и количеством курсов
+    const students = await prisma.student.findMany({
+        include: {
+            person: true,
+            grades: {
+                select: { courseId: true }
+            }
+        }
+    });
+
+    const studentsWithCounts = students.map(student => {
+        const uniqueCourses = new Set(student.grades.map(g => g.courseId));
+        return {
+            ...student,
+            courseCount: uniqueCourses.size
+        };
+    });
+
+    const maxCount = Math.max(...studentsWithCounts.map(s => s.courseCount));
+
+    return studentsWithCounts.filter(student => 
+        student.courseCount === maxCount && student.courseCount > 0
+    );
 }
