b6942c770902ea6d6c594fe2851918e0831cf0d0
klfkglf <freedom_bob@mail.ru>
2026-01-21 06:09:43 +0600

    p26

--- src/prisma/prisma26.ts
+++ src/prisma/prisma26.ts
@@ -3,4 +3,27 @@ import { prisma } from './prisma_init'
 export async function find_oldest_and_newest_students() {
     // TODO: Найти самого старого и самого нового студента по дате создания
     // Вернуть объект { oldest: студент, newest: студент } с информацией о person
+    const oldest = await prisma.student.findFirst({
+        include: { 
+            person: true 
+        },
+        orderBy: { 
+            person: {
+                createdAt: 'asc'
+            }
+        }
+    });
+
+    const newest = await prisma.student.findFirst({
+        include: { 
+            person: true 
+        },
+        orderBy: { 
+            person: {
+                createdAt: 'desc'
+            }
+        } 
+    });
+    
+    return { oldest, newest };
 }
