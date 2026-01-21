604c57a072fabf1efea210ac24fbf20e3ae7acc5
klfkglf <freedom_bob@mail.ru>
2026-01-21 06:41:24 +0600

    p29

--- src/prisma/prisma29.ts
+++ src/prisma/prisma29.ts
@@ -4,4 +4,34 @@ export async function find_students_with_same_names() {
     // TODO: Найти студентов с одинаковыми именами
     // Использовать обработку в TypeScript
     // Вернуть массив групп студентов с одинаковыми именами
+    const allStudents = await prisma.student.findMany({
+        include: {
+            person: true
+        }
+    });
+
+    const nameGroups: { [key: string]: any[] } = {};
+    
+    for (const student of allStudents) {
+        const name = student.person.name;
+        
+        if (!nameGroups[name]) {
+            nameGroups[name] = [];
+        }
+        
+        nameGroups[name].push(student);
+    }
+
+    const result = [];
+    
+    for (const name in nameGroups) {
+      if (nameGroups[name].length > 1) {
+            result.push({
+                name: name,
+                students: nameGroups[name]
+            });
+        }
+    }
+    
+    return result;
 }
