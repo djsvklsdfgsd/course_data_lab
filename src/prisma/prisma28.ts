5ec9625c12255724b5a95e71ff6a5409b9e7b3fa
klfkglf <freedom_bob@mail.ru>
2026-01-21 06:24:41 +0600

    p28

--- src/prisma/prisma28.ts
+++ src/prisma/prisma28.ts
@@ -4,4 +4,13 @@ export async function delete_students_before_date(date: Date) {
     // TODO: Удалить всех студентов, созданных до указанной даты
     // Использовать один запрос deleteMany средствами Prisma
     // Вернуть количество удаленных записей
+    const result = await prisma.student.deleteMany({
+        where: {
+            createdAt: {
+                lt: date
+            }
+        }
+    });
+    
+    return result.count;
 }
